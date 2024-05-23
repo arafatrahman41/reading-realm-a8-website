ae046b969ec9797a7e8baeeee06d9412a2211e3d
arafatrahman41 <arafatrahman41@outlook.com>
2024-05-23 10:25:22 +0600

    start working on list card

---
+++ src/components/ReadBook.jsx
@@ -0,0 +1,10 @@
+const ReadBook = ({book}) => {
+    console.log(book);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ReadBook;
--- src/components/ReadBooks.jsx
+++ src/components/ReadBooks.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
+import { getBooks } from "../utils";
+import ReadBook from "./ReadBook";
 
 const ReadBooks = () => {
-    return (
-        <div>
-            <h1>here goes read books</h1>
-        </div>
-    );
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        const storedBooks = getBooks();
+        setBooks(storedBooks)
+    }, [])
+  return (
+    <div>
+      <ul className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-24">
+        {books.map((book) => (
+          <ReadBook key={book.id} book={book}></ReadBook>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ReadBooks;
